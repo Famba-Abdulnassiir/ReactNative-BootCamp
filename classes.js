5af317f38a0fc80606ee1a3e9e8cc9aa0c9bd70a
FambaAbdulnassiirSiraji <fnassiir22@gmail.com>
2021-04-24 06:35:01 +0300

    react concepts like state, props and components before diving into react native

---
+++ classes.js
@@ -0,0 +1,6 @@
+/*syntax introduced in Es6
+    it simplifies the defining of complex objects with their own prototyoes
+    classes vs instences
+    Methods vs static methods vs properties
+    new, contsructor, extends, super are the terms we are about to see so often in classes*/
+
